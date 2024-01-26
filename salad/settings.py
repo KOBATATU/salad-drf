a4f0a54a1e996dfd950b58c130757f9b58e5c247
KOBATATU <kobatatu258@yahoo.co.jp>
2024-01-26 19:58:36 +0900

    add: drf_spectacularのswaggerエンドポイントを追加

--- salad/settings.py
+++ salad/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     #3rd party
     'rest_framework',
+    'drf_spectacular',
     # app
     'user',
     'userProfile'
@@ -112,6 +113,18 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    # YOUR SETTINGS
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+#3rd party
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
